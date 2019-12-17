f0a5735beb2adb455f355f59bbecab38deb9d4a8
Julio Collado <jcollado@altimetrik.com>
2019-12-16 23:22:34 -0300

    SpaceBattle: Base Project

--- SpaceBattle/GameScene.swift
+++ SpaceBattle/GameScene.swift
@@ -8,82 +8,168 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var starField: SKEmitterNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    let torpedoSoundAction: SKAction = SKAction.playSoundFileNamed("torpedo.mp3", waitForCompletion: false)
+    var gameTimer: Timer!
+    var attackers = ["meteor","alien"]
+
+    let alienCategory: UInt32 = 0x1 << 1
+    let torpedoCategory: UInt32 = 0x1 << 0
+    
+    let motionManager = CMMotionManager()
+    var xAcceleration: CGFloat = 0
+    
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        setupStarField()
+        setupPlayer()
+        setupPhisicsWord()
+        setupScoreLabel()
+        setupAliensAndAsteroids()
+    }
+    
+    func setupCoreMotion() {
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data: CMAccelerometerData?, error: Error?) in
+            if let accelerometerData = data {
+                let acceleration = accelerometerData.acceleration
+                self.xAcceleration = CGFloat(acceleration.x) * 0.75 + self.xAcceleration * 0.25
+            }
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func setupPhisicsWord() {
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+    func setupStarField() {
+        starField = SKEmitterNode(fileNamed: "Starfield")
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        starField.position = CGPoint(x: 0, y: self.frame.maxY)
+        starField.advanceSimulationTime(10)
+        addChild(starField)
+        starField.zPosition = -1
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func setupPlayer() {
+        player = SKSpriteNode(imageNamed: "spaceship")
+        player.size = CGSize(width: 80, height: 80)
+        player.position = CGPoint(x: frame.size.width / 2, y: player.size.height / 2 + 20)
+        addChild(player)
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func setupScoreLabel() {
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: frame.size.width - 100, y: frame.size.height - 70)
+        scoreLabel.fontSize = 25
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        scoreLabel.color = .white
+        addChild(scoreLabel)
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func setupAliensAndAsteroids() {
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addAliensAndAsteroids), userInfo: nil, repeats: true)
     }
     
+    @objc func addAliensAndAsteroids() {
+        //Shuffled array of attackers
+        attackers = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: attackers) as! [String]
+        let attaker = SKSpriteNode(imageNamed: attackers[0])
+        let attakerPosition = GKRandomDistribution(lowestValue: 0, highestValue: Int(frame.size.width))
+        let position = CGFloat(attakerPosition.nextInt())
+        attaker.size = CGSize(width: 60, height: 60)
+        attaker.position = CGPoint(x: position, y: frame.size.height + attaker.size.height)
+        attaker.physicsBody = SKPhysicsBody(circleOfRadius: attaker.size.width/2)
+        
+        attaker.physicsBody?.categoryBitMask = alienCategory
+        attaker.physicsBody?.contactTestBitMask = torpedoCategory
+        attaker.physicsBody?.collisionBitMask = 0
+        
+        addChild(attaker)
+        
+        let animationDuration: TimeInterval = 6
+        
+        var actionArray = [SKAction]()
+        actionArray.append(SKAction.move(to: CGPoint(x: position, y: -attaker.size.height), duration: animationDuration))
+        actionArray.append(SKAction.removeFromParent())
+        
+        attaker.run(SKAction.sequence(actionArray))
+    }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireTorpedo()
+    }
+    
+    func fireTorpedo() {
+        let torpedoNode = SKSpriteNode(imageNamed: "torpedo")
+        torpedoNode.position = player.position
+        torpedoNode.position.y += 5
+        torpedoNode.size = CGSize(width: 30, height: 30)
+        torpedoNode.physicsBody = SKPhysicsBody(circleOfRadius: torpedoNode.size.width/2)
+        
+        torpedoNode.physicsBody?.categoryBitMask = torpedoCategory
+        torpedoNode.physicsBody?.contactTestBitMask = alienCategory
+        torpedoNode.physicsBody?.collisionBitMask = 0
+        torpedoNode.physicsBody?.usesPreciseCollisionDetection = true
+        
+        addChild(torpedoNode)
+        
+        let animationDuration = 1.0
+        
+        var actionArray = [SKAction]()
+        actionArray.append(torpedoSoundAction)
+        actionArray.append(SKAction.move(to: CGPoint(x: player.position.x, y: frame.size.height + torpedoNode.size.height), duration: animationDuration))
+        actionArray.append(SKAction.removeFromParent())
+        torpedoNode.run(SKAction.sequence(actionArray))
+    }
+}
+
+extension GameScene: SKPhysicsContactDelegate {
+    func didBegin(_ contact: SKPhysicsContact) {
+        torpedoDidCollideWithAlien(body1: contact.bodyA.node as! SKSpriteNode, body2: contact.bodyB.node as! SKSpriteNode)
+    }
+    
+    func torpedoDidCollideWithAlien(body1: SKSpriteNode, body2: SKSpriteNode) {
+        let explosion = SKEmitterNode(fileNamed: "Explosion")!
+        explosion.position = body1.position
+        addChild(explosion)
+        
+        run(SKAction.playSoundFileNamed("explosion.mp3", waitForCompletion: false))
+        
+        body1.removeFromParent()
+        body2.removeFromParent()
+        
+        run(SKAction.wait(forDuration: 1)) {
+            explosion.removeFromParent()
+        }
+        score += 5
+    }
+    
+    override func didSimulatePhysics() {
+        player.position.x += xAcceleration * 50
+        if player.position.x < -80 {
+            player.position = CGPoint(x: CGFloat(frame.size.width + 80), y: player.position.y)
+        } else if player.position.x > frame.size.width  + 80 {
+            player.position = CGPoint(x: CGFloat(frame.size.width - 80), y: player.position.y)
+        }
     }
 }
